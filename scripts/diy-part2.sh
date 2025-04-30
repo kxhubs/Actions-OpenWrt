c17b5bd9fcc737db4abcfe06e91e28daa13921f0
Kxhubs <hqwlcm@gmail.com>
2025-04-30 22:10:38 +0800

    修改文件路径位置，增加插件

---
+++ scripts/diy-part2.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+
+#默认主题
+WRT_THEME: alpha
+#默认主机名
+WRT_NAME: Kxhubs
+#默认地址
+WRT_IP: 192.168.1.2
+
+WRT_DATE=$(TZ=UTC-8 date +"%y.%m.%d_%H.%M.%S")
+WRT_MARK=$(echo $GITHUB_REPOSITORY | cut -d '/' -f 1)
+CFG_FILE="./package/base-files/files/bin/config_generate"
+
+#修改默认主题
+sed -i "s/luci-theme-bootstrap/luci-theme-$WRT_THEME/g" $(find ./feeds/luci/collections/ -type f -name "Makefile")
+
+#修改immortalwrt.lan关联IP
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/$WRT_IP/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+
+#添加编译日期标识
+sed -i "s/(\(luciversion || ''\))/(\1) + (' \/ $WRT_MARK-$WRT_DATE')/g" $(find ./feeds/luci/modules/luci-mod-status/ -type f -name "10_system.js")
+
+#修改默认主机名
+sed -i "s/hostname='.*'/hostname='$WRT_NAME'/g" $CFG_FILE
